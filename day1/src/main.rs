d9763134cf1f2e8435213c72a1907bb43d1ec5ad
Ben (X13/Arch) <ben@wolkenwelten.net>
2022-12-01 11:49:27 +0100

    Added some tests

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() {
-    let file = File::open("input.txt").unwrap();
+fn run(path:&str) -> (i32, i32) {
+    let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
     let mut elves:Vec<i32> = vec![0];
     for line in reader.lines() {
@@ -14,8 +14,23 @@ fn main() {
         }
     }
     elves.sort();
-    let most = *elves.last().unwrap();
-    let top_three = elves.iter().rev().take(3).fold(0,|a,c|a+c);
+    (*elves.last().unwrap(), elves.iter().rev().take(3).fold(0,|a,c|a+c))
+}
+
+fn main() {
+    let (most, top_three) = run("input.txt");
     println!("Most calories carried by a single elf: {}", most);
     println!("Calories carried by the top three elves: {}", top_three);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day_one() {
+        let (most, top_three) = run("input.txt");
+        assert_eq!(most, 74394);
+        assert_eq!(top_three, 212836);
+    }
 }
